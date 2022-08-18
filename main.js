0797c790629347dd6f0f1c68a0d7d2da05b19ca5
glopezgaschetto <gonzalolg.lopez@gmail.com>
2022-08-18 19:44:45 -0300

    subida a github

---
+++ main.js
@@ -0,0 +1,28 @@
+let comoestuvolaclase = prompt("como estuvo la clase del 1 al 5?");
+
+
+
+    switch (comoestuvolaclase) {
+        case "1":console.log("mala")
+
+            break;
+        case "2": console.log("no tan mala")
+
+            break;
+        case "3": console.log("bien")
+
+            break;
+        case "4": console.log("muy bien")
+
+            break;
+        case "5": console.log("excelente")
+
+            break;
+        default: console.log("no hay nota")
+            break;
+    }
+
+
+
+
+
